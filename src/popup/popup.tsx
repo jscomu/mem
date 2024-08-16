03d0a89bdb84011831f93a592a1b7e7cd8588916
stones <comujs@gmail.com>
2024-08-16 09:32:27 +0900

    work2

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import { useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './popup.css'
 import ComCard from './comCard'
@@ -16,6 +17,8 @@ const App: React.FC<{}> = () => {
 
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choice1st, setChoice1st] = useState(null)
+  const [choice2rd, setChoice2rd] = useState(null)
 
   const btnExe = () => {
     //카드 섞기
@@ -28,7 +31,26 @@ const App: React.FC<{}> = () => {
   }
 
   const handleChoice = (card) => {
-    console.log(card)
+    //console.log(card)
+    choice1st ? setChoice2rd(card) : setChoice1st(card)
+  }
+
+  useEffect(() => {
+    if(choice1st && choice2rd) {  //카드 두개가 선택되어 있으면
+      if(choice1st.src === choice2rd.src) {
+        console.log("그림이 같다.")
+        resetTurn()
+      } else {
+        console.log("그림이 다르다.")
+        resetTurn()
+      }
+    }
+  }, [choice1st, choice2rd])
+
+  const resetTurn = () => {
+    setChoice1st(null)
+    setChoice2rd(null)
+    setTurns(prevTurns => prevTurns + 1)
   }
 
   return (
